2a95206562eb2b311676999dc245386f1dd7beed
AndyofStateFarm <andrewjb97@yahoo.com>
2024-05-20 19:28:49 -0400

    Test accelerate and brake methods on Car instances.

--- cc_3.js
+++ cc_3.js
@@ -17,4 +17,25 @@ class car{
     brake(){
         this.speed -= 5;
         console.log(`${this.make} braked to slow down 5 km/h. Speed is now ${this.speed} km/h`);
+    }
 }
+
+// Test Cars
+let car1 = new car("BMW", 120);
+let car2 = new car("Mercedes", 95);
+
+// Test Car 1
+console.log(`${car1.make} is going ${car1.speed} km/h`);
+car1.brake();
+car1.brake();
+car1.accelerate();
+car1.brake();
+car1.accelerate();
+
+// Test Car 
+console.log(`\n${car2.make} is going ${car2.speed} km/h`);
+car2.accelerate();
+car2.accelerate();
+car2.brake();
+car2.brake();
+car2.accelerate();
