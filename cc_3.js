84ead899cb1715cc4490881b1061a81e6d32e757
AndyofStateFarm <andrewjb97@yahoo.com>
2024-05-20 17:30:08 -0400

    Create Car class with constructor for make and speed.

---
+++ cc_3.js
@@ -0,0 +1,9 @@
+// U08282838
+
+// Created car class with a constructor for make and speed
+class car{
+    constructor(make, speed){
+        this.make = make;
+        this.speed = speed;
+    }
+}
