4aec00fa460ff2154cddd100f115dd82402b2336
AndyofStateFarm <andrewjb97@yahoo.com>
2024-05-20 19:12:49 -0400

    Add accelerate method to increase speed by 10 km/h.

--- cc_3.js
+++ cc_3.js
@@ -1,9 +1,15 @@
 // U08282838
 
-// Created car class with a constructor for make and speed
+// Created car class with a constructor for make(brand) and speed(km/h)
 class car{
     constructor(make, speed){
         this.make = make;
         this.speed = speed;
+    }
+
+    // Accelerate car's speed by 10 km/h
+    accelerate(){
+        this.speed += 10;
+        console.log(`${this.make} accelerated 10 km/h. Speed is now ${this.speed} km/h`);
     }
 }
